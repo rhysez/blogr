1ea056077b8e8930f730e07f05cabd3dc277a8c6
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2023-12-31 16:22:00 +0000

    create appController.js and require dependencies

--- models/post.js
+++ models/post.js
@@ -1,42 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema(
+const PostSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  text: {
+    type: String,
+    required: true,
+  },
+
+  date: {
+    type: String,
+  },
+
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+
+  comments: [
     {
-        title: {
-            type: String,
-            required: true
-        },
+      text: {
+        type: String,
+        required: true,
+      },
 
-        text: {
-            type: String,
-            required: true
-        },
+      date: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
 
-        date: {
-            type: String,
-        },
-
-        user: {
-            type: Schema.Types.ObjectId,
-            required: true
-        },
-
-        comments: [
-            {
-                text: {
-                    type: String,
-                    required: true
-                },
-
-                date: {
-                    type: String,
-                    required: true
-                }
-            }
-        ],
-    }
-);
-
-module.exports = mongoose.model('Post', PostSchema)
+module.exports = mongoose.model("Post", PostSchema);
--- models/user.js
+++ models/user.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    first_name: { type: String, required: true, maxLength: 25 },
-    last_name: { type: String, required: true, maxLength: 25 },
-    user_name: { type: String, required: true, maxlength: 100 },
-    password: { type: String, required: true, maxLength: 75 },
+  first_name: { type: String, required: true, maxLength: 25 },
+  last_name: { type: String, required: true, maxLength: 25 },
+  user_name: { type: String, required: true, maxlength: 100 },
+  password: { type: String, required: true, maxLength: 75 },
 });
 
-UserSchema.virtual('full_name', function() {
-    let fullName = '';
+UserSchema.virtual("full_name", function () {
+  let fullName = "";
 
-    if (this.first_name && this.last_name) {
-        fullName = `${this.first_name} ${this.last_name}`
-    };
+  if (this.first_name && this.last_name) {
+    fullName = `${this.first_name} ${this.last_name}`;
+  }
 
-    return fullName
-})
+  return fullName;
+});
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
